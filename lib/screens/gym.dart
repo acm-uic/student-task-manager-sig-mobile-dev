322a55fa38f10d235176aebed56ae2edd8ff7b98
DaniyalSKKR <122335626+DaniyalSKKR@users.noreply.github.com>
2024-10-18 21:42:17 -0500

    First commit, set local paths

--- lib/screens/gym.dart
+++ lib/screens/gym.dart
@@ -1,24 +1,46 @@
 import 'package:flutter/material.dart';
 import '../widgets/general/bottom_tab_navigator.dart';
 
-
 class GymPage extends StatelessWidget {
-  const GymPage({ super.key });
+  const GymPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return (
-        Scaffold (
-          appBar: AppBar(
-            title: Text("UIC Task Manager"),
-            backgroundColor: Colors.red,
-            centerTitle: true,
+    return Scaffold(
+      // Top App Bar section
+      appBar: AppBar(
+        title: const Text("UIC Task Manager"),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      body: Stack(
+        // stack layers widgets
+        children: [
+          Align(
+            alignment: Alignment.topLeft,
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: GestureDetector(
+                onTap: () {
+                  // Update for a drop-down later
+                },
+                child: const Text(
+                  "Monday",
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
           ),
-          body: const Center (
-            child: Text ("Let's get them gains!")
+
+          // Centered Main Text
+          const Center(
+            child: Text("Let's get them gains!"),
           ),
-          bottomNavigationBar: bottomTabNavigator(),
-        )
-      );
-    }
+        ],
+      ),
+
+      // Bottom Navigation Bar
+      bottomNavigationBar: bottomTabNavigator(),
+    );
   }
+}
