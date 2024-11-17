4ae43144fd8a128697299682395d3af512005949
Owen Gutierrez <161267035+oguti3@users.noreply.github.com>
2024-11-17 01:05:36 -0600

    Update home.dart
    Added the Sections and Subsections in Home Screen that is scrollable, currently hard coded to have 5 sections each with 5 subsections. Subsections have the ability to be tapped.

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -8,16 +8,56 @@ class MainHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold (
       appBar: AppBar(
-          title: Text("UIC Task Manager"),
+          title: const Text("UIC Task Manager"),
           backgroundColor: Colors.red,
           centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          "Welcome to the UIC Student Task Manager!"
-        ),
+      body: Center(
+        child: sectionList(),
       ),
       bottomNavigationBar: bottomTabNavigator(),
     ); // placeholder text in center of the screen
   }
+}
+
+Widget sectionList() {
+  return ListView.builder(
+    itemCount: 5,
+    itemBuilder: (context, sectionIndex) {
+      return Column(
+        children: [
+          ListTile(
+            title: Text('Section $sectionIndex'),
+          ),
+          subsectionList(sectionIndex),
+        ],
+      );
+    }
+  );
+}
+
+Widget subsectionList(int sectionIndex) {
+  return ListView.builder(
+    shrinkWrap: true,
+    physics: const ClampingScrollPhysics(),
+    itemCount: 5,
+    itemBuilder: (context, subsectionIndex) {
+      return subsectionTask(subsectionIndex);
+    },
+  );
+}
+
+Widget subsectionTask(int subsectionIndex) {
+  return Card(
+    clipBehavior: Clip.hardEdge,
+    child: InkWell(
+      splashColor: Colors.red.withAlpha(30),
+      onTap: () {
+        debugPrint('Card $subsectionIndex tapped.'); /* if needed: clicking on card goes here */
+      },
+      child: ListTile(
+        title: Text('Task $subsectionIndex'),
+      ),
+    ),
+  );
 }
